b7906d12731b5a4d42d703c6a4df9f18f0f21fc3
Nikoloz <nikoloz@bojgua.com>
2018-12-30 22:32:09 +0300

    It works!

---
+++ data.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ==============
+# Доп информация
+# ==============
+
+msg1 = "Lets do it.."
+
+url = 'http://192.168.1.195/'
+on = 'socket2Off'
+off = 'socket2On'
+g1 = 'socket1Off'
+g2 = 'socket1On'
--- homeBot.py
+++ homeBot.py
@@ -1,41 +1,62 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# ==============
 # Телеграм бот - умный дом
+# ==============
+
+# Импорт встроенных библиотек
+import requests
+import time
+
 # Импорт библиотек
 import telebot
 from telebot import apihelper
 
 # Импорт файлов
 import constants
+import data
 
 # Настройки
 token = constants.token_Nick420
 apihelper.proxy = {'https': 'https://{}'.format(constants.proxy)}
 
-mymsg = "Howdy, how are you doing?"
-
 # Функции
 # Отправка клавиатуры меню
-def menu_markup():
+def switch_markup():
     menu_markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
-    menu_markup.row('shit')
+    menu_markup.row('On', 'Off')
+    menu_markup.row('Гараж')
     return menu_markup
 
 # Токен
 bot = telebot.TeleBot(token)
 
+# Обертки
+# Ответы на комманды
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-#	bot.reply_to(message, "Howdy, how are you doing?")
-    bot.send_message(message.from_user.id, mymsg, reply_markup=menu_markup())
+    bot.send_message(message.from_user.id, data.msg1, reply_markup=switch_markup())
 
+# Ответ на текстовые сообщения
 @bot.message_handler(func=lambda message: True,
                      content_types=['text'])
 def default_command(message):
-    if message.text == 'shit':
-        bot.send_message(message.chat.id, "получен")
+    if message.text == 'On' and message.from_user.id == constants.my_id:
+        requests.get(data.url + data.on)
+        bot.send_message(constants.my_id, "Is on!")
+    elif message.text == 'Off' and message.from_user.id == constants.my_id:
+        requests.get(data.url + data.off)
+        bot.send_message(constants.my_id, "Is off!")
+    elif message.text == 'Гараж':
+        requests.get(data.url + data.g1)
+        time.sleep(1)
+        requests.get(data.url + data.g2)
+        bot.send_message(constants.my_id, "Гараж в действии!")
 
 
-bot.polling(none_stop = 'True', interval = 0)			    					# Запуск цикла
+# Запуск главного цикла
+if __name__ == '__main__':
+    bot.polling(none_stop = 'True')
 
 """
 Все что после, не выполняется
